4e4fbe0f11c4172aa70886866dfe5e067a724e3a
Evan Barton <notrabe1011@gmail.com>
2020-08-13 18:30:33 -0400

    completed previous project and today's

--- src/App.js
+++ src/App.js
@@ -1,14 +1,38 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./App.css";
+import axios from 'axios';
+import Header from './header';
+import Body from './body';
+
+import styled from 'styled-components'
+
+const StyledApp = styled.div`
+background-image:linear-gradient(purple, pink);
+`
 
 function App() {
+
+  const [info, setInfo] = useState('')
+
+  useEffect(() => {
+    
+    axios.get('https://api.nasa.gov/planetary/apod?api_key=IUZ6Fh56qQeOncqgjPOFRwYYCmenZKKHZqbeV1GO')
+  
+    .then(res => {
+      setInfo(res.data)
+    })
+    .catch(err => {
+      debugger
+    })
+
+  },[])
+ 
+
   return (
-    <div className="App">
-      <p>
-        Read through the instructions in the README.md file to build your NASA
-        app! Have fun <span role="img" aria-label='go!'>🚀</span>!
-      </p>
-    </div>
+    <StyledApp className="App">
+      <Header date = {info.date}/>
+      <Body img = {info.url} text = {info.explanation}/>
+    </StyledApp>
   );
 }
 
